6a6480318a6979ffc22c93ef3feca7fcb0e72b1d
AdnanHassan2003 <cadnaanhassan2003@gmail.com>
2025-05-28 23:33:56 +0300

    added some changes for patient prescription

--- application/telemedicine-controller/telemedicine-controller.js
+++ application/telemedicine-controller/telemedicine-controller.js
@@ -6030,7 +6030,6 @@ exports.patient_prescription = function(req, res){
             $match: {
                 "patient_id": ObjectId(req.body.patient_id),
                  "doctor_id": ObjectId(req.body.doctor_id),
-                 "appointment_id":ObjectId(req.body.appointment_id)
             }
         },
 
