4a38c16ecf7cc5b9bfc5834e2502c408d0565811
AdnanHassan2003 <cadnaanhassan2003@gmail.com>
2025-07-09 10:29:06 +0300

    added api get all appointment

--- application/telemedicine-controller/telemedicine-controller.js
+++ application/telemedicine-controller/telemedicine-controller.js
@@ -7222,7 +7222,9 @@ exports.allAppointments = function(req, res) {
             $project: {
                 _id: 1,
                 doctor_name: "$doctor_data.name",
+                doctor_token: "$doctor_data.token",
                 patient_name: "$patient_data.name",
+                patient_token: "$patient_data.token",
                 appointment_date: 1,
                 status: 1,
                 create_date: 1,
